bf6921040907c213fc92f20abd59ecb5db1828a3
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-02-03 17:39:38 +0800

    Update derp_dipper.mk

--- derp_dipper.mk
+++ derp_dipper.mk
@@ -9,13 +9,16 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 # Inherit some common DerpFest stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+CURRENT_BUILD_TYPE := gapps
+DERP_BUILDTYPE := Official
+
+
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
 IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-FORCE_JSON=1
-DERP_BUILDTYPE := Official
-CURRENT_BUILD_TYPE := gapps
+endif
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_dipper
