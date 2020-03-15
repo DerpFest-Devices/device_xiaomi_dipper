1dac7c2674e17567724263fc2c733b0dfee2dc6a
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-03-16 07:49:19 +0800

    dipper: Use simple kernel
    lol change to simple kernel aaaaagain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_USES_COLOR_METADATA := true
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/dipper
-TARGET_KERNEL_CONFIG := dipper_defconfig
+TARGET_KERNEL_CONFIG := revenge_defconfig
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
